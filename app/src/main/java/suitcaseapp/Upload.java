e3046cc57247c16c61ce4babf36ae5bb9ebb9d18
Patrick <59621160+patrickekeu@users.noreply.github.com>
2024-09-30 02:38:41 +0100

    Add files via upload

--- app/src/main/java/suitcaseapp/Upload.java
+++ app/src/main/java/suitcaseapp/Upload.java
@@ -1,1 +1,79 @@
+package com.example.suitcaseapp;
 
+import android.net.Uri;
+
+import com.google.firebase.Timestamp;
+
+
+public class Upload {
+    private String name;
+    private String mImageUrl;
+
+    private String title;
+    private String price;
+    private String desc;
+    private Boolean purchased = false;
+    private String image;
+    private Timestamp timeAdded;
+
+    public Upload()
+    {
+        // Empty constructor needed
+    }
+    public Upload(String name, String imageUrl)
+    {
+        if(name.trim().equals(""))
+        {
+            name = "No Name";
+        }
+        this.name = name;
+        mImageUrl = imageUrl;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public boolean isPurchased()
+    {
+       return purchased;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public Timestamp getTimeAdded() {
+        return timeAdded;
+    }
+
+    public String getPrice()
+    {
+        return price;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getImage()
+    {
+        return image;
+    }
+
+    public void setUri(String image)
+    {
+        this.image = image;
+    }
+
+    public void setImageUrl(String imageUrl)
+    {
+        mImageUrl = imageUrl;
+    }
+}
